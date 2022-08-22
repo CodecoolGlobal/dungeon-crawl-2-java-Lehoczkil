0ca9bb539ca6ee24f73e671c2db2d949150234e5
RRicsi89 <rozsar1989@gmail.com>
2022-08-22 19:43:14 +0200

    Add alter table ... drop cascade to be able to delete tables

--- sample_data/schema_ddl.sql
+++ sample_data/schema_ddl.sql
@@ -1,3 +1,7 @@
+ALTER TABLE IF EXISTS ONLY public.game_state DROP CONSTRAINT IF EXISTS fk_player_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.items DROP CONSTRAINT IF EXISTS fk_player_id CASCADE;
+
+
 DROP TABLE IF EXISTS public.game_state;
 CREATE TABLE public.game_state (
     id serial NOT NULL PRIMARY KEY,
@@ -19,12 +23,13 @@ ALTER TABLE ONLY public.game_state
     ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);
 
 DROP TABLE IF EXISTS public.inventory;
+DROP TABLE IF EXISTS public.items;
 
-CREATE TABLE public.inventory (
+CREATE TABLE public.items (
     player_id INTEGER NOT NULL,
     name VARCHAR(30) NOT NULL,
     quantity INTEGER
 );
 
-ALTER TABLE ONLY public.inventory
+ALTER TABLE ONLY public.items
     ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);

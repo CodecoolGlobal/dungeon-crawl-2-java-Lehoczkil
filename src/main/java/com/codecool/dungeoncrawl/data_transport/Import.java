9f602f067cf4e0a8c33ffe8f5c0b975cc987e870
pitchforx <m.lorandlevente@gmail.com>
2022-08-24 14:23:49 +0200

    Implement Import class

--- src/main/java/com/codecool/dungeoncrawl/data_transport/Import.java
+++ src/main/java/com/codecool/dungeoncrawl/data_transport/Import.java
@@ -1,41 +1,39 @@
 package com.codecool.dungeoncrawl.data_transport;
 
-
 import com.codecool.dungeoncrawl.model.GameState;
 import com.google.gson.Gson;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-public class Export {
+public class Import {
 
-    private GameState gameState;
     private Stage primaryStage;
 
-
-    public Export(GameState gameState, Stage primaryStage) {
-        this.gameState = gameState;
+    public Import(Stage primaryStage) {
         this.primaryStage = primaryStage;
     }
 
-    public void exportGame() {
+    public GameState importGame() {
         FileChooser fc = new FileChooser();
         fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("JSON", "*.json"));
-        File file = fc.showSaveDialog(primaryStage);
+        File file = fc.showOpenDialog(primaryStage);
         if (file != null) {
-            String path = file.getPath();
-            try (PrintWriter out = new PrintWriter(new FileWriter(path))) {
+            try(Reader reader = Files.newBufferedReader(Paths.get(file.getPath()))) {
                 Gson gson = new Gson();
-                String jsonString = gson.toJson(gameState);
-                out.write(jsonString);
+                return gson.fromJson(reader, GameState.class);
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
         }
+        return null;
     }
 
 }

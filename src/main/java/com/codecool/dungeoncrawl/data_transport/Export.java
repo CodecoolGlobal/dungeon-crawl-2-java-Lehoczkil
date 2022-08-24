e65cbb8c465ee8b2aaf00e30b0371ffa89a54271
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-08-24 10:36:42 +0200

    Fix export and add new constructor for test purposes

--- src/main/java/com/codecool/dungeoncrawl/data_transport/Export.java
+++ src/main/java/com/codecool/dungeoncrawl/data_transport/Export.java
@@ -1,6 +1,7 @@
 package com.codecool.dungeoncrawl.data_transport;
 
 
+import com.codecool.dungeoncrawl.model.GameState;
 import com.google.gson.Gson;
 
 import java.io.FileWriter;
@@ -9,18 +10,23 @@ import java.io.PrintWriter;
 
 public class Export {
 
-    private Container container;
+    private GameState gameState;
     private String path;
 
-    public Export(Container container, String path) {
-        this.container = container;
+    public Export(GameState gameState, String path) {
+         this.gameState= gameState;
         this.path = path;
+    }
+
+    public Export(GameState gameState) {
+        this.gameState = gameState;
+        this.path = "src/main/resources/test.json";
     }
 
     public void export() {
         try (PrintWriter out = new PrintWriter(new FileWriter(path))) {
             Gson gson = new Gson();
-            String jsonString = gson.toJson(container);
+            String jsonString = gson.toJson(gameState);
             out.write(jsonString);
         } catch (IOException e) {
             e.printStackTrace();

a0404d5e85b35af879fe767323fcab05836a0e33
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-08-25 13:58:35 +0200

    Add Test for Enemy

---
+++ src/test/java/com/codecool/dungeoncrawl/logic/EnemyTest.java
@@ -0,0 +1,31 @@
+package com.codecool.dungeoncrawl.logic;
+import com.codecool.dungeoncrawl.logic.actors.Boss;
+import com.codecool.dungeoncrawl.logic.actors.Enemy;
+import com.codecool.dungeoncrawl.logic.actors.Player;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EnemyTest {
+    GameMap gameMap = new GameMap(3,3,CellType.FLOOR);
+
+    @Test
+    void creatingEnemyInstanceDoesNotThrowException() {
+        assertDoesNotThrow(() -> new Enemy(gameMap.getCell(2,1)));
+    }
+
+    @Test
+    void enemyReturnsCorrectName() {
+        Enemy enemy = new Enemy(gameMap.getCell(2,1));
+        assertEquals("enemy", enemy.getTileName());
+    }
+
+    @Test
+    void enemyCanAttackOtherActors() {
+        Enemy enemy = new Enemy(gameMap.getCell(2,1));
+        Player player = new Player(gameMap.getCell(1,1));
+        enemy.attack(player);
+        assertEquals(8,player.getHealth());
+    }
+
+}

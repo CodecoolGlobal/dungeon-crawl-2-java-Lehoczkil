768577a01a4f855deb6b35337111278a4b510007
RRicsi89 <rozsar1989@gmail.com>
2022-08-25 14:09:17 +0200

    Implement unittests for MapLoader class

--- src/test/java/com/codecool/dungeoncrawl/logic/GameMapTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/GameMapTest.java
@@ -8,8 +8,6 @@ import com.codecool.dungeoncrawl.logic.items.Door;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
